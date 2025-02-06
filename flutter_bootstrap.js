1522d54ab382f64d7e6520263c32bef04c34a80f
Great Leh Baedee <gbaedee@gmail.com>
2025-02-06 14:36:30 -0600

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3784629831"
+    serviceWorkerVersion: "2690679229"
   }
 });
